1f1a9f32fffdd36007157a7678b099ecb701a145
StDuindam <34353940+StDuindam@users.noreply.github.com>
2017-12-12 21:46:27 +0100

    Two easy GameObject spawn mechanics
    Decide where you deploy your gameobjects, with ranges from amount per row to amount of rows etc.

---
+++ Formationspawners/RandomFormationSpawner.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFormationSpawner : MonoBehaviour {
+
+    public GameObject Enemy;
+    List<GameObject> Enemies = new List<GameObject>();
+
+    public int spawnAmount;
+    public int leftToSpawn;
+
+    public int spawnX;
+
+    public int minX;
+    public int maxX;
+
+    public int minZ;//Diepte
+    public int maxZ;//Aantal rijen
+
+    //Lets get it started
+	void Start ()
+    {
+        minX = -20;
+        maxX = 20;     
+	}
+
+    //Calculate all the enemies
+    public void Calculate()
+    {
+        leftToSpawn = spawnAmount - Enemies.Count;
+    }
+
+    //Set everything up
+    public void Phase1()
+    {
+        //Create a safetynet to not crash unity
+        int totalX = (minX * -1) + maxX;
+        int totalZ = (minZ *-1) + maxZ;
+        int totalRange = (totalX * totalZ);
+        Debug.Log(totalRange);
+        //Calculate how many enemies to spawn
+        Calculate();
+        //Make sure it is possible to give every object a unique location
+        if (spawnAmount < totalRange && maxZ > 0)
+        {
+            Debug.Log("Possible");
+            //Create all enemies, add them to the list, set inactive to keep them on hold
+            for (int i = 0; i < leftToSpawn; i++)
+            {
+                GameObject go = Instantiate(Enemy, Vector3.zero, Quaternion.identity);
+                Enemies.Add(go);
+            }
+            //Reset the speed of the already existing enemies
+            for (int i = 0; i < Enemies.Count; i++)
+            {
+                Enemies[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                //Give all the enemies a unique position
+                UniquePosition(Enemies[i]);
+                Enemies[i].SetActive(false);
+            }
+         }else{
+            Debug.Log("Too much blocks to spawn or no depth!");
+       }
+    }
+
+    //Time to spawn all enemies!
+    public void Phase2()
+    {
+        for(int i = 0; i < spawnAmount; i++)
+        {
+            Enemies[i].SetActive(true);
+        }
+    }
+
+    //A function requiring the gameobject to assign a position to each object
+    public void UniquePosition(GameObject go)
+    {
+        //create a boolean to alert us when a position is used and a new position for the game object
+        bool noCopy = false;
+        Vector3 uniquepos = new Vector3(Random.Range(minX, maxX), 0, Random.Range(minZ, maxZ));
+
+        //Check if the new position is not already in use
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            //if its in use it gets stopped and the cycle gets restarted
+            if (uniquepos == Enemies[i].transform.position)
+            {
+                UniquePosition(go);
+                Debug.Log("Trying again");
+                return;
+            }
+
+            //if its not in use the boolean stays false
+            else
+            {
+                noCopy = false;
+            }
+        }
+
+        //if at the end of the for loop the boolean is false, the position is assigned
+        if (!noCopy)
+        {
+            go.transform.position = uniquepos;
+        }     
+    }
+ }
---
+++ Formationspawners/SpawnFormation.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFormation : MonoBehaviour {
+
+    //Load enemy
+    public GameObject enemy;
+    //List<GameObject> Enemies = new List<GameObject>();
+
+    //Positions
+    private int startPosX;
+    private float endPosX;
+
+    private int startPosZ;
+    private int endPosZ;
+
+    //Amount of enemies
+    private int amountToSpawn;
+
+    //A random function to test the script through a button. This can also be called upon from other scripts
+    public void button() {
+        Setup(2, 1, 4, 30);
+    }
+    
+    //Setup the spawnformation
+    public void Setup(int startPositionX, int startPositionZ, int rows, int spawnAmount) {
+        if (rows <= spawnAmount && rows > 0) {
+            //Assign basic variables and information
+            startPosX = startPositionX;
+            startPosZ = startPositionZ;
+            amountToSpawn = spawnAmount;
+
+            //Define the start and end position, round the end position down
+            endPosX = Mathf.Floor(spawnAmount / rows);
+            float endingposition = startPosX + endPosX;
+
+            Debug.Log(endPosX);
+
+            //Safetynet so no game objects spawn in eachothers location
+            if (endPosX > 2) {
+                for (int i = 0; i < spawnAmount; i++) {
+                    if (startPosX < endingposition)//Enemy amount / rows = amount of object per row, this has to be checked an can't be 0
+                    {
+                        //first object has to be set on exactly 0,0 point
+                        if (i == 0) {
+                            Instantiate(enemy, new Vector3(startPositionX, 0, startPosZ), Quaternion.identity);
+                        }
+                        //If its not the first one start moving spawn locations
+                        else {
+                            startPosX += 1;
+                            Instantiate(enemy, new Vector3(startPosX, 0, startPosZ), Quaternion.identity);
+                        }
+                    }
+                    //if the row is full, proceed to the next line on the Z-Axis
+                    else {
+                        startPosX = startPositionX;
+                        startPosZ += 1;
+                        Instantiate(enemy, new Vector3(startPosX, 0, startPosZ), Quaternion.identity);
+                    }
+                }
+            }
+            else {
+                //Make sure it is possible to spawn the right amount of enemies and rows
+                Debug.Log("too few enemies and too many rows!"); 
+            }
+        }
+        else {
+            //Make sure you don't multiply or divide anything by 0
+            Debug.Log("Must have atleast 1 row, spawn amount can't be smaller than amount of rows"); } 
+    }
+}
