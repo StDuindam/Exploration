071c216323bc77e8b540d1fb969c3645932498f5
Sten Duindam <34353940+StDuindam@users.noreply.github.com>
2017-12-17 18:56:33 +0100

    XML_Data

---
+++ XML_Datasave/Data_Analysis.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("PlayerdataCollection")]
+public class Data_Analysis : MonoBehaviour {
+
+    //Data to print & read
+    [SerializeField]
+    private string Name;
+    //Int seconds
+    [SerializeField]
+    private float secondsPlayed;
+    //Actions done
+    [SerializeField]
+    private int actionsDone;
+    //Old highscore
+    [SerializeField]
+    private int oldHighscore;
+    [SerializeField]
+    private int actionsPerSecond;
+    [SerializeField]
+    private int currentSession;
+    public PlayerDataContainer DataList;
+
+    void Awake() {
+        //SetData();
+        }
+    
+	// Use this for initialization
+	void Start () {
+        LoadData();
+        SetSessionNumber();
+        oldHighscore = GetOldHighscore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        secondsPlayed += Time.deltaTime;
+	}
+
+    public void AddActionsDone() {
+        actionsDone += 1;
+    }
+
+    public int SetSessionNumber() {
+        if(DataList.PlayerDataList != null) {
+            currentSession = DataList.PlayerDataList.Count +1;
+            }
+        else { currentSession = 1; }
+        return currentSession;
+        }
+
+    public int GetOldHighscore() {
+        int tempInt = DataList.PlayerDataList.Count;
+        for(int i = 0; i < tempInt; i++) {
+            if(oldHighscore < DataList.PlayerDataList[i].actionsCompleted) {
+                oldHighscore = DataList.PlayerDataList[i].actionsCompleted;
+                }
+            }
+        return oldHighscore;}
+
+   public void SaveCurrentSession() {
+        PlayerData thisSession = new PlayerData();
+        thisSession.Name = Name;
+        thisSession.SessionNumber = currentSession;
+        thisSession.actionsCompleted = actionsDone;
+        thisSession.ActionPerSecond = ActionsPerSecondCalc();
+        DataList.PlayerDataList.Add(thisSession);
+        }
+
+    public int ActionsPerSecondCalc() {
+        float tempint;
+        if (secondsPlayed != 0 || actionsDone != 0) {
+            tempint = secondsPlayed / actionsDone;
+            }
+        else { tempint = 0; }
+        return Mathf.RoundToInt(tempint);
+        }
+
+    public void SaveData() {
+        SaveCurrentSession();
+        XmlSerializer serializer = new XmlSerializer(typeof(PlayerDataContainer));
+        FileStream stream = new FileStream(Application.dataPath + "/Resources/XML_v1.xml", FileMode.OpenOrCreate);
+        serializer.Serialize(stream, DataList);
+        stream.Close();
+        Debug.Log("saved");
+        }
+
+    public void LoadData() {
+        XmlSerializer serializer = new XmlSerializer(typeof(PlayerDataContainer));
+        FileStream stream = new FileStream(Application.dataPath + "/Resources/XML_v1.xml", FileMode.Open);
+        DataList = serializer.Deserialize(stream) as PlayerDataContainer;
+        stream.Close();
+        Debug.Log("Loaded");
+        }
+    public void OnApplicationQuit() {
+        if (secondsPlayed > 5) {
+            SaveData();
+            }
+        }
+}
+
+[System.Serializable]
+public class PlayerData {
+    [XmlAttribute("name")]
+    public string Name;
+    public int SessionNumber;
+    public int actionsCompleted;
+    public int ActionPerSecond;
+    }
+
+[System.Serializable]
+public class PlayerDataContainer {
+    [XmlArray("PlayerDataArray")]
+    [XmlArrayItem("Playerdata")]
+    public List<PlayerData> PlayerDataList = new List<PlayerData>();
+    }
---
+++ XML_Datasave/Input_Script.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+    //Visual jumptext
+    [SerializeField]
+    private Text jumpNumbers;
+    private int jumpNum;
+
+    //The data i want to print and read!
+    
+    
+    //basic stuff
+    public bool isGrounded = true;
+    private bool isJumping = false;
+    private Vector3 up = new Vector3(0,1,0);
+    [SerializeField]
+    private Animator animator;
+    private Rigidbody rbody;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        jumpNum = 0;
+	}
+	
+	//Input
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            Jump();
+            isGrounded = false;
+            }
+	}
+
+    //Jump up!
+    public void Jump() {
+        isJumping = true;
+        rbody.AddForce(up * RandomJumpForce());
+        animator.SetBool("Jump", true);
+        animator.SetBool("Grounded", false);
+        jumpNum += 1;
+        jumpNumbers.text = ("Jumps: " + jumpNum);
+        }
+
+    //Back on the ground
+    public void OnTriggerEnter(Collider col) {
+        if (isJumping) {    
+            animator.SetBool("Jump", false);
+            animator.SetBool("Grounded", true);
+            isGrounded = true;
+            isJumping = false;
+            }
+        }
+    private int RandomJumpForce() {
+        return Random.Range(75, 200);
+        }
+}
