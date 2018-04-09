03309fd3b25dd8cac1b102938ae03ff4a3f2a5e9
Sten_Desktop <sten.duindam@gmail.com>
2018-04-09 13:59:01 +0200

    XML and JSON comparison
    This project tries out the same actions with two different data structures.

---
+++ XML_VS_JSON_Example/JSON_Data.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+
+public class JSON_Data : MonoBehaviour {
+    //Created a string to make sure no mistakes are made when typing this path
+    private string path = "data.json";
+
+    public void Start() {
+        //Make sure there is a file, if not create it.
+        if (!File.Exists(path)) {
+            Debug.Log("JSON file does not exist, creating..");
+            JSON_PlayerDataList JSON_list = new JSON_PlayerDataList();
+            JSON_SerializerWriter.JSONSerialize(JSON_list, path);
+            }
+        NewInstance();
+        }
+    
+    //Function to create new instances to add to the list
+    public void NewInstance() {
+        JSON_PlayerDataList JSON_list = JSON_SerializerWriter.JSONDeserialize<JSON_PlayerDataList>(path);//Load the already existing list
+        JSON_PlayerData JSON_data = new JSON_PlayerData();//Create a new data set
+        JSON_data.Name = ("Jason" + JSON_list.Json_List.Count + 1);//Assign values
+        JSON_data.ActionPerSecond = Random.Range(0, 200);
+        JSON_data.actionsCompleted = Random.Range(0, 200);
+        JSON_data.SessionNumber = JSON_list.Json_List.Count + 1;
+        JSON_list.Json_List.Add(JSON_data);//Add the new set to the list of data
+        JSON_SerializerWriter.JSONSerialize(JSON_list, path); // write all the data to the .json file
+        Debug.Log("Amount of JSON item(s): " + JSON_list.Json_List.Count);
+        //Debug.Log("A random session number from the list: " + JSON_list.Json_List[Random.Range(0,JSON_list.Json_List.Count)].SessionNumber);
+        }
+
+    }
+//The data to store
+    public class JSON_PlayerData {
+        public string Name;
+        public int SessionNumber;
+        public int actionsCompleted;
+        public int ActionPerSecond;
+        }
+   //The list that contains all data
+    public class JSON_PlayerDataList {
+        public List<JSON_PlayerData> Json_List = new List<JSON_PlayerData>();
+        }
+   //OOP Json serializer and dezerializer
+public class JSON_SerializerWriter {
+
+    public static void JSONSerialize(object _item, string _path) {
+        using (StreamWriter file = File.CreateText(_path)) {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, _item);
+            }
+        }
+
+    public static T JSONDeserialize<T>(string _path) {
+        T item = JsonConvert.DeserializeObject<T>(File.ReadAllText(_path));
+        return item;
+        }
+    }
+
+//######### NOTES #########
+// Also interesting: Population(https://www.newtonsoft.com/json/help/html/PopulateObject.htm), this writes all file contained values to a class object
+//https://www.newtonsoft.com/json/help/html/DeserializeWithJsonSerializerFromFile.htm
+/*using (StreamReader file = File.OpenText(_path)) {
+    JsonSerializer serializer = new JsonSerializer();
+    T playerdata = (T)serializer.Deserialize(file, typeof(T));
+    }*/
+
---
+++ XML_VS_JSON_Example/XML_Data.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+using System.Xml;
+//using System.Globalization;
+
+public class XML_Data : MonoBehaviour{
+    
+    //No declared variables what so ever needed, just easy to create a string for the path
+    private string loc = "xmlList.xml"; 
+
+	void Start () {
+        //A check to see if the file exists or not, create one if not.
+        if (!File.Exists(loc)) {
+            Debug.Log("XML file does not exist, creating...");
+            XML_PlayerDataList xml_list = new XML_PlayerDataList();
+            XML_SerializerWriter.XMLSerialize(xml_list, loc);//replaced the path for a string variable, this way it is harder to make mistakes
+            }
+        NewInstance();
+        //Debug.Log(System.DateTime.Now);
+	}
+	
+	// Function to create a new set of data and assigning these
+	void NewInstance () {  
+        XML_PlayerDataList xml_list = XML_SerializerWriter.XMLDeserialize<XML_PlayerDataList>(loc); //Load the existing items into our list
+        XML_PlayerData xml_data = new XML_PlayerData(); //Create new playerdata
+        xml_data.Name = ("Sam" + xml_list.XML_List.Count + 1) ; //set some variables
+        xml_data.ActionPerSecond = Random.Range(0,200);
+        xml_data.actionsCompleted = Random.Range(0, 200);
+        xml_data.SessionNumber = xml_list.XML_List.Count + 1;
+        xml_list.XML_List.Add(xml_data); // add this instance to the list
+        XML_SerializerWriter.XMLSerialize(xml_list, loc); //save the list back into the file
+        Debug.Log("Amount of XML item(s): "+ xml_list.XML_List.Count);
+        }
+}
+//The data to store
+[XmlRoot("Instance")]
+public class XML_PlayerData {
+    [XmlElement("name")]
+    public string Name;
+    [XmlAttribute("Session no")]
+    public int SessionNumber;
+    public int actionsCompleted;
+    public int ActionPerSecond;
+    }
+
+//A list to hold all the data(in this example I made a list containing all player data)
+public class XML_PlayerDataList {
+    [XmlArray("Container")]
+    public List<XML_PlayerData> XML_List = new List<XML_PlayerData>();
+    }
+
+//OOP design to create a XML serializer and writer without specifying typing, this way it can be accessed anywhere to create any file holding any type
+public class XML_SerializerWriter {
+
+    public static void XMLSerialize(object item, string path) {
+        XmlSerializer serializer = new XmlSerializer(item.GetType());
+        StreamWriter writer = new StreamWriter(path);
+        serializer.Serialize(writer.BaseStream, item);
+        writer.Close();
+        }
+
+    public static T XMLDeserialize<T>(string path) {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        StreamReader reader = new StreamReader(path);
+        T deserialized = (T)serializer.Deserialize(reader.BaseStream);
+        reader.Close();
+        return deserialized;
+        }
+    }
